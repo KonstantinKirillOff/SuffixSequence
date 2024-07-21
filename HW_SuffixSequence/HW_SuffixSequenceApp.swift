0da71f700316db9230842f30d1051cb0db6b6376
Konstantin Kirillov <kirilloffkostya@gmail.com>
2024-07-21 20:49:49 +0700

    Initial Commit

---
+++ HW_SuffixSequence/HW_SuffixSequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_SuffixSequenceApp.swift
+//  HW_SuffixSequence
+//
+//  Created by Konstantin Kirillov on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW_SuffixSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
