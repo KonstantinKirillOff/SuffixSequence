e558a25f6ba0629e495133dd1717eab0052e9ab5
Konstantin Kirillov <kirilloffkostya@gmail.com>
2024-07-22 22:04:28 +0700

    add viewModel

--- SuffixSequence/Modules/SuffixesViewModel.swift
+++ SuffixSequence/Modules/SuffixesViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by Konstantin Kirillov on 22.07.2024.
 //
 
-import Foundation
+import Combine
+
+final class SuffixesViewModel: ObservableObject {
+    @Published var wordsString = ""
+    @Published var searchString = ""
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        
+    }
+    
+    private func setFilterForItems() {
+        
+    }
+    
+    private func getSuffixesForString() {
+        
+    }
+}
